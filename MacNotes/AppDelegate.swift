cd1e9e075d567c9ea83fb00bf90e4f77cc6055e4
Antony Yurchenko <AntonyBrro@gmail.com>
2017-04-11 22:52:33 +0300

    Initial Commit

---
+++ MacNotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacNotes
+//
+//  Created by Antony Yurchenko on 11/4/17.
+//  Copyright © 2017 Antony Yurchenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ MacNotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacNotes
+//
+//  Created by Antony Yurchenko on 11/4/17.
+//  Copyright © 2017 Antony Yurchenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
